f587e2553a1e436e18715a937d2ac6a03d7a0042
Wei Huang <weih@kahoot.com>
2021-04-21 11:05:42 +0200

    Accept crowdin token from inputs

--- lib/mix/tasks/crowdin.ex
+++ lib/mix/tasks/crowdin.ex
@@ -1,11 +1,18 @@
 defmodule Mix.Tasks.Crowdin do
   use Mix.Task
 
+  defp get_crowdin_token() do
+    case System.get_env("INPUT_CROWDIN_TOKEN") do
+      val when val in [nil, ""] -> System.get_env("INPUT_TOKEN")
+      val -> val
+    end
+  end
+
   def run([workspace]) do
     IO.puts "Mix crowdin task #{inspect workspace}"
 
     organization = System.get_env("INPUT_ORGANIZATION")
-    token = System.get_env("INPUT_TOKEN")
+    token = get_crowdin_token()
     project_id = System.get_env("INPUT_PROJECT_ID")
     source_file = System.get_env("INPUT_SOURCE_FILE")
     update_source = System.get_env("INPUT_UPDATE_SOURCE")
